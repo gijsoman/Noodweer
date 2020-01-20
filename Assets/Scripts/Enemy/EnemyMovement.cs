df9e0201d7473dcbad6845f876dc4b511e3328ac
Gijs Bakker <gijs.bakker@student.hku.nl>
2020-01-20 14:22:31 +0100

    So far so good. Everything runs smoothly in build.

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(Rigidbody), typeof(CapsuleCollider))]
+public class EnemyMovement : MonoBehaviour
+{
+    public WOWCamera wowCam;
+    public float RotationSpeed;
+
+    private Animator animator;
+    private Rigidbody rb;
+
+    private float Vertical;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Vertical = Input.GetAxis("Vertical");
+
+        Move();
+    }
+
+    private void FixedUpdate()
+    {
+        Rotate();
+    }
+
+    private void Move()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            animator.SetBool("Run", true);
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            animator.SetBool("Run", false);
+
+        animator.SetFloat("Forward", Vertical);
+    }
+
+    private void Rotate()
+    {
+        if (Input.GetMouseButton(1) && wowCam != null)
+        {
+            transform.eulerAngles = new Vector3(0, wowCam.transform.eulerAngles.y, 0);
+        }
+        else if (animator.GetFloat("Forward") == 0)
+        {
+            // set animator bool to use rotate animations.
+        }
+        else if(Input.GetAxis("Horizontal") != 0)
+        {
+            transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * RotationSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class EnemyScript : MonoBehaviour
+{
+    public delegate void EnemyEvent();
+    public EnemyEvent IDied;
+
+    private bool alive = true;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Bullet_45mm_Bullet(Clone)")
+        {
+            if(alive)
+                anim.SetTrigger("Death");
+            alive = false;
+            IDied?.Invoke();
+        }
+    }
+
+    private void OnDestroy()
+    {        
+        IDied = null;
+    }
+}
