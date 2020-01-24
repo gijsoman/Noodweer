b595a774f593695b9a81258d9e9447148e782811
Gijs Bakker <gijs.bakker@student.hku.nl>
2020-01-24 16:34:15 +0100

    New fmod upload. Soundboard is ready to go!

---
+++ Assets/EnemySoundBoard.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class EnemySoundBoard : MonoBehaviour
+{
+    private StudioEventEmitter[] eventEmitters;
+
+    private void Start()
+    {
+        eventEmitters = GetComponents<StudioEventEmitter>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1) && Input.GetKey(KeyCode.LeftShift)){
+            LookupCorrectEmitter("event:/Voice_lines/Ik_Wil_Niet_Meer");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1)){
+            LookupCorrectEmitter("event:/Voice_lines/Schiet");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2) && Input.GetKey(KeyCode.LeftShift)){
+            LookupCorrectEmitter("event:/Voice_lines/Ik_Wil_Geen_Hulp");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)){
+            LookupCorrectEmitter("event:/Voice_lines/Schiet_Dan");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) && Input.GetKey(KeyCode.LeftShift)){
+            LookupCorrectEmitter("event:/Voice_lines/Frustratie");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3)){ 
+            LookupCorrectEmitter("event:/Voice_lines/Ja_Precies");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4) && Input.GetKey(KeyCode.LeftShift)){
+            LookupCorrectEmitter("event:/Voice_lines/Godverdomme");
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4)){
+            LookupCorrectEmitter("event:/Voice_lines/Schiet_Godverdomme");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5) && Input.GetKey(KeyCode.LeftShift)){
+            LookupCorrectEmitter("event:/Voice_lines/Werk_Mee");}
+        else if (Input.GetKeyDown(KeyCode.Alpha5)){
+            LookupCorrectEmitter("event:/Voice_lines/Nee_Schiet");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            LookupCorrectEmitter("event:/Voice_lines/Je_Durft_Niet");
+
+        if (Input.GetKeyDown(KeyCode.E))
+            LookupCorrectEmitter("event:/Voice_lines/Kom_Dan");
+
+        if (Input.GetKeyDown(KeyCode.R))
+            LookupCorrectEmitter("event:/Voice_lines/Mik_Hier");
+    }
+
+    private void LookupCorrectEmitter(string _event)
+    {
+        for (int i = 0; i < eventEmitters.Length; i++)
+        {
+            if (eventEmitters[i].Event == _event)
+            {
+                StopAllEmitters();
+                eventEmitters[i].Play();
+            }
+        }
+    }
+
+    private void StopAllEmitters()
+    {
+        for (int i = 0; i < eventEmitters.Length; i++)
+        {
+            eventEmitters[i].Stop();
+        }
+    }
+}
--- Assets/Scripts/Player/FollowVRCam.cs
+++ Assets/Scripts/Player/FollowVRCam.cs
@@ -15,53 +15,42 @@ public class FollowVRCam : MonoBehaviour
 
     private void Update()
     {
-        transform.position = new Vector3(VRCamera.position.x + startOffset.x, VRCamera.position.y + startOffset.y, VRCamera.position.z-startOffset.z);
+        transform.position = new Vector3(VRCamera.position.x + startOffset.x, VRCamera.position.y + startOffset.y, VRCamera.position.z - startOffset.z);
 
+        HandleBodyRotation();
+    }
+
+    private void HandleBodyRotation()
+    {
         float VRcamY = VRCamera.eulerAngles.y;
+        float bodyY = transform.eulerAngles.y;
 
-
-        float bodyY = transform.eulerAngles.y;     
-        Debug.Log(bodyY + " | " + VRcamY);
-        //we want seperate if statements for different states
-
-        //if the body is at 0 we want to change when over 90 or under 270
-        if (bodyY == 0)
+        switch (bodyY)
         {
-            if (VRcamY < 270 && VRcamY > 180)
-                transform.eulerAngles = new Vector3(0, 270, 0);
-            if (VRcamY > 90 && VRcamY < 180)
-                transform.eulerAngles = new Vector3(0, 90, 0);
+            case 0:
+                if (VRcamY < 270 && VRcamY > 180)
+                    transform.eulerAngles = new Vector3(0, 270, 0);
+                if (VRcamY > 90 && VRcamY < 180)
+                    transform.eulerAngles = new Vector3(0, 90, 0);
+                break;
+            case 90:
+                if (VRcamY > 270)
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                if (VRcamY > 180 && VRcamY < 270)
+                    transform.eulerAngles = new Vector3(0, 180, 0);
+                break;
+            case 180:
+                if (VRcamY < 90)
+                    transform.eulerAngles = new Vector3(0, 90, 0);
+                if (VRcamY > 270)
+                    transform.eulerAngles = new Vector3(0, 270, 0);
+                break;
+            case 270:
+                if (VRcamY > 0 && VRcamY < 90)
+                    transform.eulerAngles = new Vector3(0, 0, 0);
+                if (VRcamY < 180 && VRcamY > 90)
+                    transform.eulerAngles = new Vector3(0, 180, 0);
+                break;
         }
-
-        if (bodyY == 90)
-        {
-            if (VRcamY > 270)
-                transform.eulerAngles = new Vector3(0, 0, 0);
-            if (VRcamY > 180 && VRcamY < 270)
-                transform.eulerAngles = new Vector3(0, 180, 0);
-        }
-
-        if (bodyY == 180)
-        {
-            if (VRcamY < 90)
-                transform.eulerAngles = new Vector3(0, 90, 0);
-            if (VRcamY > 270)
-                transform.eulerAngles = new Vector3(0, 270, 0);
-        }
-
-        if (bodyY == 270)
-        {
-            if (VRcamY > 0 && VRcamY < 90)
-                transform.eulerAngles = new Vector3(0, 0, 0);
-            if (VRcamY < 180 && VRcamY > 90)
-                transform.eulerAngles = new Vector3(0, 180, 0);
-        }
-
-
-        //if the body is at 90 we want to change when over 180 and over 270
-
-        //if the body is at 180 we want to change when over 270 and under 90
-
-        //if the body is at 270 we want to change when over 0 and under 180
     }
 }
