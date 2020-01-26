917c151129f9586447cf026ea7a840c3240cce5a
Gijs Bakker <gijs.bakker@student.hku.nl>
2020-01-26 21:36:36 +0100

    Alright the tutorialmanager has a good start. Its ugly but it works so far.

--- Assets/TutorialManager.cs
+++ Assets/TutorialManager.cs
@@ -6,39 +6,95 @@ public class TutorialManager : MonoBehaviour
 {
     public Hand leftHand;
     public Hand rightHand;
+
     public SteamVR_Action_Vector2 WalkingAction;
+    public SteamVR_Action_Boolean RotateRight;
+    public SteamVR_Action_Boolean RotateLeft;
+    public SteamVR_Action_Boolean GrabGrip;
 
-    public VRWalking VrWalking;
+    public bool playerWalked = false;
+    public bool playerTurnedRight = false;
+    public bool playerTurnedLeft = false;
+    public bool playerGrabbedGun = false;
 
-    private bool playerWalked = false;
-    private bool walkingTutorialFinished = false;
+
+    enum TutorialState
+    {
+        Walking,
+        Turning,
+        Grabbing,
+        Holstering
+    }
+
+    private TutorialState tutorialState = TutorialState.Walking;
 
     private void Update()
     {
-        if(!walkingTutorialFinished)
-            HandleWalkingHint(leftHand);
+        if(tutorialState == TutorialState.Walking)
+            HandleWalkingHint();
+
+        if (tutorialState == TutorialState.Turning)
+            HandleSnapTurnHint();
+
+        if (tutorialState == TutorialState.Grabbing)
+            HandleGrabHint();
     }
 
-    private void HandleWalkingHint(Hand hand)
+    private void HandleWalkingHint()
     {
+        if (WalkingAction.changed)
+            playerWalked = true;
+
         if (!playerWalked)
         {
-            ControllerButtonHints.ShowTextHint(hand, WalkingAction, "Raak de trackpad aan om te lopen", true);
-            hand.SetSkeletonRangeOfMotion(EVRSkeletalMotionRange.WithController, 0.1f);
+            ControllerButtonHints.ShowTextHint(leftHand, WalkingAction, "Raak de trackpad aan om te lopen", true);
+            leftHand.SetSkeletonRangeOfMotion(EVRSkeletalMotionRange.WithController, 0.1f);
         }
         else
         {
-            ControllerButtonHints.HideAllTextHints(hand);
-            hand.SetSkeletonRangeOfMotion(EVRSkeletalMotionRange.WithoutController, 0.1f);
-            walkingTutorialFinished = true;
+            ControllerButtonHints.HideAllTextHints(leftHand);
+            leftHand.SetSkeletonRangeOfMotion(EVRSkeletalMotionRange.WithoutController, 0.1f);
+            tutorialState = TutorialState.Turning;
         }
-
-        if (VrWalking.walkState == walkingState.Walking)
-            playerWalked = true;
     }
 
-    private void HandleRotationHint(Hand hand)
+    private void HandleSnapTurnHint()
     {
+        if (RotateRight.GetStateDown(SteamVR_Input_Sources.RightHand))
+        {
+            playerTurnedRight = true;
+            ControllerButtonHints.HideTextHint(rightHand, RotateRight);
+        }
+        if (RotateLeft.GetStateDown(SteamVR_Input_Sources.RightHand))
+            playerTurnedLeft = true;
 
+        if (!playerTurnedRight)
+        {
+            ControllerButtonHints.ShowTextHint(rightHand, RotateRight, "Druk aan de rechterkant van de trackpad om snel naar rechts te draaien", true);
+            rightHand.SetSkeletonRangeOfMotion(EVRSkeletalMotionRange.WithController, 0.1f);
+        }
+        else if (!playerTurnedLeft)
+        {
+            ControllerButtonHints.ShowTextHint(rightHand, RotateLeft, "Druk aan de linkerkant van de trackpad om snel naar links te draaien", true);
+            rightHand.SetSkeletonRangeOfMotion(EVRSkeletalMotionRange.WithController, 0.1f);
+        }
+        else
+        {
+            ControllerButtonHints.HideAllTextHints(rightHand);
+            rightHand
+.SetSkeletonRangeOfMotion(EVRSkeletalMotionRange.WithoutController, 0.1f);
+            tutorialState = TutorialState.Grabbing;
+        }
+    }
+
+    private void HandleGrabHint()
+    {
+        if (!playerGrabbedGun)
+        {
+            ControllerButtonHints.ShowTextHint(leftHand, GrabGrip, "Druk op de grip button om je wapen op te pakken", true);
+            ControllerButtonHints.ShowTextHint(rightHand, GrabGrip, "Druk op de grip button om je wapen op te pakken", true);
+            rightHand.SetSkeletonRangeOfMotion(EVRSkeletalMotionRange.WithController, 0.1f);
+            leftHand.SetSkeletonRangeOfMotion(EVRSkeletalMotionRange.WithController, 0.1f);
+        }
     }
 }
