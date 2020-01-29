23d1aad0373e43419e44d48defde69b39b67bfdd
Gijs Bakker <gijs.bakker@student.hku.nl>
2020-01-29 10:53:13 +0100

    Fixed an important bug and changed the level layout.

--- Assets/Scripts/Player/FollowVRCam.cs
+++ Assets/Scripts/Player/FollowVRCam.cs
@@ -22,35 +22,39 @@ public class FollowVRCam : MonoBehaviour
 
     private void HandleBodyRotation()
     {
-        float VRcamY = VRCamera.eulerAngles.y;
-        float bodyY = transform.eulerAngles.y;
+        float VRcamY = VRCamera.localEulerAngles.y;
+        float bodyY = transform.localEulerAngles.y;
+
+        Debug.Log(VRcamY + " | " + bodyY);
 
         switch (bodyY)
         {
             case 0:
                 if (VRcamY < 270 && VRcamY > 180)
-                    transform.eulerAngles = new Vector3(0, 270, 0);
+                    transform.localEulerAngles = new Vector3(0, 270, 0);
                 if (VRcamY > 90 && VRcamY < 180)
-                    transform.eulerAngles = new Vector3(0, 90, 0);
+                    transform.localEulerAngles = new Vector3(0, 90, 0);
                 break;
             case 90:
                 if (VRcamY > 270)
-                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    transform.localEulerAngles = new Vector3(0, 0, 0);
                 if (VRcamY > 180 && VRcamY < 270)
-                    transform.eulerAngles = new Vector3(0, 180, 0);
+                    transform.localEulerAngles = new Vector3(0, 180, 0);
                 break;
             case 180:
                 if (VRcamY < 90)
-                    transform.eulerAngles = new Vector3(0, 90, 0);
+                    transform.localEulerAngles = new Vector3(0, 90, 0);
                 if (VRcamY > 270)
-                    transform.eulerAngles = new Vector3(0, 270, 0);
+                    transform.localEulerAngles = new Vector3(0, 270, 0);
                 break;
             case 270:
                 if (VRcamY > 0 && VRcamY < 90)
-                    transform.eulerAngles = new Vector3(0, 0, 0);
+                    transform.localEulerAngles = new Vector3(0, 0, 0);
                 if (VRcamY < 180 && VRcamY > 90)
-                    transform.eulerAngles = new Vector3(0, 180, 0);
+                    transform.localEulerAngles = new Vector3(0, 180, 0);
                 break;
         }
+
+        
     }
 }
